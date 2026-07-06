65302060cfdfc04483dfddb0a5113221313c2e4c
exeexe6161 <exeexe6161@users.noreply.github.com>
2026-07-06 06:35:01 +0200

    Unify game design system details

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
    ============================================================ */
 'use strict';
 
-var CACHE = 'dailycode-portal-v19';
+var CACHE = 'dailycode-portal-v20';
 var PREFIX = 'dailycode-portal-';
 var LEGACY = ['dailycode-cache-v2'];
 
